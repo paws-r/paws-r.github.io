a3d23ddb416c3dad40a841e1869ee8fc6bde18b9
Larefly <dyfanjones@hotmail.com>
2023-04-05 22:15:45 +0100

    update title

--- create_pr.sh
+++ create_pr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$(Rscript get_version.R)
-pr_title="Regenerate Paws: ${version}"
+pr_title="Regenerate Paws Website: ${version}"
 pr_body="Regenerate Paws SDK Website: ${version}"
 
 gh pr create --title "${pr_title}" --body "${pr_body}"
